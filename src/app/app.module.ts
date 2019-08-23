1e473b39a7108770d3259fb9a4d0318243350978
ferdinandmwal123 <ferdinandmwal@gmail.com>
2019-08-23 12:21:16 +0300

    view commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
